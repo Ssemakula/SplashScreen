3ab40afc1f8a565f036dd73691286779e125e9a6
Ssemakula Mukiibi <Ssemakula@users.noreply.github.com>
2024-09-10 00:01:43 +0300

    Add project files.

---
+++ SplashScreen/SplashScreen.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SplashScreen
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this.splashTimer = new System.Windows.Forms.Timer(this.components);
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.statusPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // splashTimer
+            // 
+            this.splashTimer.Tick += new System.EventHandler(this.SplashTimer_Tick);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.BackColor = System.Drawing.Color.Transparent;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(45, 28);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(51, 20);
+            this.statusLabel.TabIndex = 0;
+            this.statusLabel.Text = "label1";
+            // 
+            // statusPanel
+            // 
+            this.statusPanel.BackColor = System.Drawing.Color.Transparent;
+            this.statusPanel.Location = new System.Drawing.Point(49, 65);
+            this.statusPanel.Name = "statusPanel";
+            this.statusPanel.Size = new System.Drawing.Size(714, 32);
+            this.statusPanel.TabIndex = 1;
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(813, 168);
+            this.Controls.Add(this.statusPanel);
+            this.Controls.Add(this.statusLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer splashTimer;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Panel statusPanel;
+    }
+}
---
+++ SplashScreen/SplashScreen.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplashScreen
+{
+    public partial class SplashScreen : Form
+    {
+        static SplashScreen splashScreen = null;
+        static Thread oThread = null;
+
+        //Fading
+        private double dblOpacityIncrement = .05;
+        private double dblOpacityDecrement = .08;
+        private const int TIMER_INTERVAL = 50;
+
+        //Status string
+        private string statusString;
+
+        //Status indicators
+        private double completedFraction = 0.0;
+        private Rectangle progressRect;
+
+        private int iActualTicks; 
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+            this.ClientSize = this.BackgroundImage.Size;
+            this.Opacity = 0.0;
+            splashTimer.Interval = TIMER_INTERVAL;
+            splashTimer.Start();
+        }
+
+        static public double Progress
+        {
+            get
+            {
+                if(splashScreen != null)
+                    return splashScreen.completedFraction;
+                return 1.0;
+            }
+            set
+            {
+                if(splashScreen != null)
+                    splashScreen.completedFraction = value;
+            }
+        }
+
+        static private void ShowForm()
+        {
+            splashScreen = new SplashScreen();
+            Application.Run(splashScreen);
+        }
+
+        static public void CloseForm()
+        {
+            if (splashScreen != null) 
+            { 
+                //Slowly fade
+                splashScreen.dblOpacityIncrement = -splashScreen.dblOpacityDecrement;
+            }
+            oThread = null; //No longer needed
+            splashScreen = null; //?? Better to destroy?
+            //splashScreen.Close();
+        }
+
+        static public void ShowSplashScreen()
+        {
+            //Launch once
+            if (splashScreen != null)
+                return;
+            oThread = new Thread(new ThreadStart(SplashScreen.ShowForm))
+            {
+                IsBackground = true
+            };
+            oThread.SetApartmentState(ApartmentState.STA);
+            oThread.Start();
+            while (splashScreen == null || splashScreen.IsHandleCreated)
+                System.Threading.Thread.Sleep(TIMER_INTERVAL);
+        }
+
+        private void SplashTimer_Tick(object sender, EventArgs e)
+        {
+            //Draw the filler rectangle
+            int width = (int)Math.Floor(statusPanel.ClientRectangle.Width * completedFraction);
+            int height = statusPanel.ClientRectangle.Height;
+            int x = statusPanel.ClientRectangle.X;
+            int y = statusPanel.ClientRectangle.Y;
+
+            if(dblOpacityIncrement > 0)
+            {
+                if (this.Opacity < 1)
+                    this.Opacity += dblOpacityIncrement;
+            }
+            else
+            {
+                if (this.Opacity > 0)
+                    this.Opacity += dblOpacityIncrement;
+                else
+                    this.Close();
+            }
+            statusLabel.Text = statusString;
+            if (width > 0 && height > 0)
+            {
+                progressRect = new Rectangle(x, y, width, height);
+                if (!statusPanel.IsDisposed)
+                {
+                    Graphics g = statusPanel.CreateGraphics();
+                    LinearGradientBrush background =
+                        new LinearGradientBrush(progressRect,
+                            Color.FromArgb(50, 50, 200),
+                            Color.FromArgb(150, 150, 255),
+                            LinearGradientMode.Horizontal);
+                    g.FillRectangle(background, progressRect);
+                    g.Dispose();
+                }
+            }
+        }
+
+        static public void SetStatus(string newStatus)
+        {
+            if (splashScreen == null)
+                return;
+            splashScreen.statusString = newStatus;
+        }
+    }
+}
